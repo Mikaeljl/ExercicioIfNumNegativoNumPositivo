804900e52ab2bb7c8ee569eb0ea1a7e695f27c38
Mikael de Jesus <mikaeljl824@gmail.com>
2024-09-09 20:18:10 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioIfNumNegativoNumPositivo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioIfNumNegativoNumPositivo
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int num= int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um numero:");
+          
+           
+
+            if (num < 0)
+            {
+                Console.WriteLine("numero negativo");
+            }
+
+            else
+            {
+                Console.WriteLine("numero positivo");
+            }
+           
+        }
+    }
+}
